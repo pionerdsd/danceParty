3ac91318edc028dfcac49f16875cf8d2e836e24e
pionerdsd <34263362+pionerdsd@users.noreply.github.com>
2018-03-20 20:13:55 +0300

    Add bean stingparser

--- src/test/java/by/learning/AppTest.java
+++ src/test/java/by/learning/AppTest.java
@@ -1,47 +1,11 @@
 package by.learning;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.contrib.java.lang.system.SystemOutRule;
-import org.junit.runner.RunWith;
-
-
-import static org.junit.Assert.assertNotNull;
-
-
 /**
  * Hello world!
  */
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = DanceConfig.class)
+
 public class AppTest {
-@Rule
-public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
-
-
-    @Autowired
-    private DanceFloor danceFloor;
-
-    @Autowired
-    private  DanceV2 dance;
-
-    @Test
-    public void danceShouldHaveAnyDanceFloor(){
-        assertNotNull(danceFloor);
-    }
-    @Test
-    public void danceShoulHaveAnyDancer(){
-        assertNotNull(dance);
-    }
-    @Test
-    public void dannce(){
-        dance.dance();
-
-    }
 
 
 
